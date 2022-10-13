b3a3e77956c7a2608f91fa61bea7a3d3f7db0369
Christopher Sloss <ttg1989_89@protonmail.com>
2022-10-13 18:30:38 -0400

    commit tree 47799fff0c1b5fe26ca0f6ed3e9fed2073fae618

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+addingEventListener(); // Call the event listener function
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+        alert('I was clicked!');
+    })
 }
+
